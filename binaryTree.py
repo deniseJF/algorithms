419776297e44ec762189e79fb72a14dabdf41093
denise <denise.j.ferreira@gmail.com>
2015-03-10 22:45:45 -0300

    ADD: remove node and balancing

--- binaryTree.py
+++ binaryTree.py
@@ -17,6 +17,29 @@ class BinaryNode:
             else:
                 self.right = BinaryNode(value)
 
+    def delete(self, value):
+        if self.left == self.right is None:
+            return None
+        elif self.left is None:
+            return self.right
+        elif self.right is None:
+            return self.left
+
+        """ balancing """
+        child = self.left
+        grandchild = child.right
+        if grandchild:
+            while grandchild.right:
+                child = grandchild
+                grandchild = child.right
+            self.value = grandchild.value
+            child.right = child.value
+        else:
+            self.left = child.left
+            self.value = child.value
+
+        return self
+
 
 class BinaryTree:
 
@@ -34,11 +57,28 @@ class BinaryTree:
         while node:
             if target == node.value:
                 return True
-            if target < node.value:
+            elif target < node.value:
                 node = node.left
             else:
                 node = node.right
         return False
+
+    def remove(self, value):
+        if self.root:
+            self.root = self.removeFromParent(self.root, value)
+
+    def removeFromParent(self, parent, value):
+        if parent is None:
+            return None
+
+        if value == parent.value:
+            return parent.delete(value)
+        elif value < parent.value:
+            parent.left = self.removeFromParent(parent.left, value)
+        else:
+            parent.right = self.removeFromParent(parent.right, value)
+
+        return parent
 
 
 import random
